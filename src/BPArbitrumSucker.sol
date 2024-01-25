dc6e55c776771752fc467860f99b0b2c15389805
xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-01-25 01:33:48 +0100

    feat: _isRemotePeer check for ARB L1 and L2

--- src/BPArbitrumSucker.sol
+++ src/BPArbitrumSucker.sol
@@ -1,35 +1,48 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-import {OPMessenger} from "./interfaces/OPMessenger.sol";
+import "@arbitrum/nitro-contracts/src/bridge/IInbox.sol";
+import "@arbitrum/nitro-contracts/src/bridge/IOutbox.sol";
+
+import {ArbSys} from "@arbitrum/nitro-contracts/src/precompiles/ArbSys.sol";
+import {AddressAliasHelper} from "@arbitrum/nitro-contracts/src/libraries/AddressAliasHelper.sol";
 
 import "./BPSucker.sol";
 
+
 /// @notice A contract that sucks tokens from one chain to another.
 /// @dev This implementation is designed to be deployed on two chains that are connected by an OP bridge.
-contract BPOptimismSucker is BPSucker {
+contract BPArbitrumSucker is BPSucker {
     using MerkleLib for MerkleLib.Tree;
     using BitMaps for BitMaps.BitMap;
+
+    enum Layer {
+        L1,
+        L2
+    }
 
     //*********************************************************************//
     // --------------- public immutable stored properties ---------------- //
     //*********************************************************************//
 
-    /// @notice The messenger in use to send messages between the local and remote sucker.
-    OPMessenger public immutable OPMESSENGER;
+    Layer public immutable LAYER;
+
+    IInbox public immutable INBOX;
 
     //*********************************************************************//
     // ---------------------------- constructor -------------------------- //
     //*********************************************************************//
     constructor(
-        OPMessenger _messenger,
+        Layer _layer,
+        address _inbox,
         IJBDirectory _directory,
         IJBTokens _tokens,
         IJBPermissions _permissions,
         address _peer,
         uint256 _projectId
     ) BPSucker(_directory, _tokens, _permissions, _peer, _projectId) {
-        OPMESSENGER = _messenger;
+        LAYER = _layer;
+        INBOX = IInbox(_inbox);
     }
 
     //*********************************************************************//
@@ -55,41 +68,41 @@ contract BPOptimismSucker is BPSucker {
         if(_tokenConfig.remoteToken == address(0))
             revert TOKEN_NOT_CONFIGURED(_token);
 
-        if(_token != JBConstants.NATIVE_TOKEN){
-            // Approve the tokens to be bridged.
-            SafeERC20.forceApprove(IERC20(_token), address(OPMESSENGER), _amount);
+        // if(_token != JBConstants.NATIVE_TOKEN){
+        //     // Approve the tokens to be bridged.
+        //     SafeERC20.forceApprove(IERC20(_token), address(OPMESSENGER), _amount);
 
-            // Bridge the tokens to the payer address.
-            OPMESSENGER.bridgeERC20To({
-                localToken: _token,
-                remoteToken: _tokenConfig.remoteToken,
-                to: PEER,
-                amount: _amount,
-                minGasLimit: _tokenConfig.minGas,
-                extraData: bytes('')
-            });
-        } else {
-            _nativeValue = _amount;
-        }
+        //     // Bridge the tokens to the payer address.
+        //     OPMESSENGER.bridgeERC20To({
+        //         localToken: _token,
+        //         remoteToken: _tokenConfig.remoteToken,
+        //         to: PEER,
+        //         amount: _amount,
+        //         minGasLimit: _tokenConfig.minGas,
+        //         extraData: bytes('')
+        //     });
+        // } else {
+        //     _nativeValue = _amount;
+        // }
 
-        // Send the messenger to the peer with the redeemed ETH.
-        OPMESSENGER.sendMessage{value: _nativeValue}(
-            PEER,
-            abi.encodeCall(
-                BPSucker.fromRemote,
-                (
-                    MessageRoot({
-                        token: _tokenConfig.remoteToken,
-                        amount: _amount,
-                        remoteRoot: RemoteRoot({
-                            nonce: _nonce,
-                            root: outbox[_token].tree.root()
-                        })
-                    })
-                )
-            ),
-            MESSENGER_BASE_GAS_LIMIT
-        );
+        // // Send the messenger to the peer with the redeemed ETH.
+        // OPMESSENGER.sendMessage{value: _nativeValue}(
+        //     PEER,
+        //     abi.encodeCall(
+        //         BPSucker.fromRemote,
+        //         (
+        //             MessageRoot({
+        //                 token: _tokenConfig.remoteToken,
+        //                 amount: _amount,
+        //                 remoteRoot: RemoteRoot({
+        //                     nonce: _nonce,
+        //                     root: outbox[_token].tree.root()
+        //                 })
+        //             })
+        //         )
+        //     ),
+        //     MESSENGER_BASE_GAS_LIMIT
+        // );
     }
 
     /// @notice checks if the _sender (msg.sender) is a valid representative of the remote peer. 
@@ -97,6 +110,15 @@ contract BPOptimismSucker is BPSucker {
     function _isRemotePeer(
         address _sender
     ) internal override returns (bool _valid) {
-        return _sender == address(OPMESSENGER) && OPMESSENGER.xDomainMessageSender() == PEER;
+        // We are the L1 peer.
+        if(LAYER == Layer.L1) {
+            IBridge _bridge = INBOX.bridge();
+            // Check that the sender is the bridge and that the outbox has our peer as the sender.
+            return _sender == address(_bridge) &&
+                address(PEER) == IOutbox(_bridge.activeOutbox()).l2ToL1Sender();
+        }
+
+        // We are the L2 peer.
+        return _sender == AddressAliasHelper.applyL1ToL2Alias(address(PEER));
     }
 }
--- src/interfaces/ArbInbox.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.21;
-
-interface ArbInbox {
-    function bridge() external view returns (IBridge);
-}
