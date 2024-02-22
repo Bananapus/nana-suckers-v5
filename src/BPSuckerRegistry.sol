9ff36f003d02fc30ce7b1c96eb25445480f661fe
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-02-22 21:43:37 +0100

    feat: added access control for new sucker registry

--- src/BPSuckerRegistry.sol
+++ src/BPSuckerRegistry.sol
@@ -2,30 +2,51 @@
 pragma solidity ^0.8.13;
 
 import {IBPSuckerRegistry, SuckerDeployerConfig, IBPSucker, BPTokenConfig} from "./interfaces/IBPSuckerRegistry.sol";
+import {JBOwnable, IJBProjects, IJBPermissions} from "@bananapus/ownable/src/JBOwnable.sol";
 
-contract BPSuckerRegistry is IBPSuckerRegistry {
+contract BPSuckerRegistry is JBOwnable, IBPSuckerRegistry {
+
+    error INVALID_DEPLOYER(address _deployer);
+
+    // TODO: Replace with correct permission id.
+    uint8 constant DEPLOY_SUCKERS_PERMISSION_ID = 100;
+
     mapping(uint256 projectId => IBPSucker[]) internal _suckersOf;
 
     mapping(address deployer => bool) public suckerDeployerIsAllowed;
+
+    constructor(
+        IJBProjects _projects,
+        IJBPermissions _permissions
+    ) JBOwnable(_projects, _permissions) {
+        // Transfer ownership to projectID 1 owner (JBDAO).
+        _transferOwnership(address(0), uint88(1));
+    }
 
     function suckersOf(uint256 projectId) external view returns (IBPSucker[] memory) {
         return _suckersOf[projectId];
     }
 
-    function allowSuckerDeployer(address deployer) public override {
-        // TODO onlyOwner // jbdao.
+    function allowSuckerDeployer(address deployer) public override onlyOwner {
         suckerDeployerIsAllowed[deployer] = true;
     }
 
     function deploySuckersFor(
         uint256 projectId,
         bytes32 salt,
-        SuckerDeployerConfig[] memory configurations
+        SuckerDeployerConfig[] calldata configurations
     )
         public
         override
     {
-        // TODO requirePermissions from owner of projectId or Operator of a new DEPLOY_SUCKERS permission.
+        _requirePermissionFrom({
+            account: PROJECTS.ownerOf(projectId),
+            projectId: projectId,
+            permissionId: DEPLOY_SUCKERS_PERMISSION_ID
+        });
+
+        // This makes it so the sender has to be the same for both chains in order to link projects.
+        salt = keccak256(abi.encode(msg.sender, salt));
 
         // Keep a reference to the number of sucker deployers.
         uint256 numberOfSuckerDeployers = configurations.length;
@@ -38,7 +59,8 @@ contract BPSuckerRegistry is IBPSuckerRegistry {
             configuration = configurations[i];
 
             // Make sure the deployer is allowed.
-            if (!suckerDeployerIsAllowed[address(configuration.deployer)]) revert();
+            if (!suckerDeployerIsAllowed[address(configuration.deployer)])
+                revert INVALID_DEPLOYER(address(configuration.deployer));
 
             // Create the sucker.
             IBPSucker sucker = configuration.deployer.createForSender({_localProjectId: projectId, _salt: salt});
