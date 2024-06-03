a84b758612c70432b7293b5facfb74ffa4dbd7d2
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2024-06-03 23:48:39 +0200

    ci: bump deployment nonce, bump npm version, added new artifacts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -25,13 +25,9 @@ contract DeployScript is Script, Sphinx {
 
     function configureSphinx() public override {
         // TODO: Update to contain JB Emergency Developers
-        sphinxConfig.owners = [0x26416423d530b1931A2a7a6b7D435Fac65eED27d];
-        sphinxConfig.orgId = "cltepuu9u0003j58rjtbd0hvu";
         sphinxConfig.projectName = "nana-suckers";
-        sphinxConfig.threshold = 1;
         sphinxConfig.mainnets = ["ethereum", "optimism", "base", "arbitrum"];
         sphinxConfig.testnets = ["ethereum_sepolia", "optimism_sepolia", "base_sepolia", "arbitrum_sepolia"];
-        sphinxConfig.saltNonce = 2;
     }
 
     function run() public {
