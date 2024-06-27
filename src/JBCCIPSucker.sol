cef522fec23b75bededc3b6813686926c470a663
simplemachine92 <justinpulley92@gmail.com>
2024-06-26 23:06:22 -0500

    CCIPSucker- compiles

---
+++ src/JBCCIPSucker.sol
@@ -0,0 +1,196 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {BitMaps} from "@openzeppelin/contracts/utils/structs/BitMaps.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IJBPrices} from "@bananapus/core/src/interfaces/IJBPrices.sol";
+import {IJBRulesets} from "@bananapus/core/src/interfaces/IJBRulesets.sol";
+import {IJBDirectory} from "@bananapus/core/src/interfaces/IJBDirectory.sol";
+import {IJBTokens} from "@bananapus/core/src/interfaces/IJBTokens.sol";
+import {IJBPermissions} from "@bananapus/core/src/interfaces/IJBPermissions.sol";
+import {JBConstants} from "@bananapus/core/src/libraries/JBConstants.sol";
+
+import {IJBCCIPSuckerDeployer} from "src/interfaces/IJBCCIPSuckerDeployer.sol";
+import {JBSucker, IJBSuckerDeployer, JBAddToBalanceMode} from "./JBSucker.sol";
+import {JBMessageRoot} from "./structs/JBMessageRoot.sol";
+import {JBRemoteToken} from "./structs/JBRemoteToken.sol";
+import {JBInboxTreeRoot} from "./structs/JBInboxTreeRoot.sol";
+import {JBCCIPSuckerDeployer} from "./deployers/JBCCIPSuckerDeployer.sol";
+import {MerkleLib} from "./utils/MerkleLib.sol";
+
+import {ModifiedReceiver} from "./utils/ModifiedReceiver.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {CCIPHelper} from "src/libraries/CCIPHelper.sol";
+
+/// @notice A `JBSucker` implementation to suck tokens between chains with Chainlink CCIP
+contract JBCCIPSucker is JBSucker, ModifiedReceiver {
+    using MerkleLib for MerkleLib.Tree;
+    using BitMaps for BitMaps.BitMap;
+
+    uint256 public remoteChainId;
+    uint64 public remoteChainSelector;
+
+    event SuckingToRemote(address token, uint64 nonce);
+
+    error NotEnoughBalance(uint256 balance, uint256 fees);
+
+    error FailedToRefundFee();
+
+    //*********************************************************************//
+    // ---------------------------- constructor -------------------------- //
+    //*********************************************************************//
+
+    constructor(
+        IJBDirectory directory,
+        IJBTokens tokens,
+        IJBPermissions permissions,
+        address peer,
+        JBAddToBalanceMode atbMode
+    ) JBSucker(directory, tokens, permissions, peer, atbMode, IJBCCIPSuckerDeployer(msg.sender).TEMP_ID_STORE()) {
+        remoteChainId = IJBCCIPSuckerDeployer(msg.sender).REMOTE_CHAIN_ID();
+        remoteChainSelector = IJBCCIPSuckerDeployer(msg.sender).REMOTE_CHAIN_SELECTOR();
+    }
+
+    //*********************************************************************//
+    // --------------------- internal transactions ----------------------- //
+    //*********************************************************************//
+
+    /// @notice
+    /// @param transportPayment the amount of `msg.value` that is going to get paid for sending this message.
+    /// @param token The token to bridge the outbox tree for.
+    /// @param remoteToken Information about the remote token being bridged to.
+    function _sendRoot(uint256 transportPayment, address token, JBRemoteToken memory remoteToken) internal override {
+        if (transportPayment == 0) {
+            revert UNEXPECTED_MSG_VALUE();
+        }
+
+        // Get the amount to send and then clear it from the outbox tree.
+        uint256 amount = outbox[token].balance;
+        delete outbox[token].balance;
+
+        // Increment the outbox tree's nonce.
+        uint64 nonce = ++outbox[token].nonce;
+
+        // Ensure the token is mapped to an address on the remote chain.
+        if (remoteToken.addr == address(0)) {
+            revert TOKEN_NOT_MAPPED(token);
+        }
+
+        bytes32 _root = outbox[token].tree.root();
+        uint256 _index = outbox[token].tree.count - 1;
+
+        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
+        Client.EVM2AnyMessage memory evm2AnyMessage = _buildCCIPMessage({
+            _receiver: address(this), // Todo: correct this
+            _root: JBMessageRoot({
+                token: remoteToken.addr,
+                amount: amount,
+                remoteRoot: JBInboxTreeRoot({nonce: nonce, root: _root})
+            }),
+            _token: token,
+            _amount: amount,
+            _feeTokenAddress: address(0) // Paid in native
+        });
+
+        // Initialize a router client instance to interact with cross-chain router
+        IRouterClient router = IRouterClient(this.getRouter());
+
+        // Get the fee required to send the CCIP message
+        uint256 fees = router.getFee({destinationChainSelector: remoteChainSelector, message: evm2AnyMessage});
+
+        if (fees > transportPayment) {
+            revert NotEnoughBalance(transportPayment, fees);
+        }
+
+        // approve the Router to spend tokens on contract's behalf. It will spend the amount of the given token
+        IERC20(token).approve(address(router), amount);
+
+        // TODO: Handle this messageId, maybe necessary
+        // Send the message through the router and store the returned message ID
+        /* messageId =  */
+        router.ccipSend{value: fees}({destinationChainSelector: remoteChainSelector, message: evm2AnyMessage});
+
+        // Refund remaining balance.
+        (bool sent,) = msg.sender.call{value: msg.value - fees}("");
+        if (!sent) revert FailedToRefundFee();
+
+        // Emit an event for the relayers to watch for.
+        emit RootToRemote(_root, token, _index, nonce);
+    }
+
+    /// @notice Construct a CCIP message.
+    /// @dev This function will create an EVM2AnyMessage struct with all the necessary information for programmable tokens transfer.
+    /// @param _receiver The address of the receiver.
+    /// @param _root The root to be sent.
+    /// @param _token The token to be transferred.
+    /// @param _amount The amount of the token to be transferred.
+    /// @param _feeTokenAddress The address of the token used for fees. Set address(0) for native gas.
+    /// @return Client.EVM2AnyMessage Returns an EVM2AnyMessage struct which contains information for sending a CCIP message.
+    function _buildCCIPMessage(
+        address _receiver,
+        JBMessageRoot memory _root,
+        address _token,
+        uint256 _amount,
+        address _feeTokenAddress
+    ) private pure returns (Client.EVM2AnyMessage memory) {
+        // Set the token amounts
+        Client.EVMTokenAmount[] memory tokenAmounts = new Client.EVMTokenAmount[](1);
+        tokenAmounts[0] = Client.EVMTokenAmount({token: _token, amount: _amount});
+        // Create an EVM2AnyMessage struct in memory with necessary information for sending a cross-chain message
+        return Client.EVM2AnyMessage({
+            receiver: abi.encode(_receiver), // ABI-encoded receiver address
+            data: abi.encode(_root), // ABI-encoded string
+            tokenAmounts: tokenAmounts, // The amount and type of token being transferred
+            extraArgs: Client._argsToBytes(
+                // Additional arguments, setting gas limit
+                Client.EVMExtraArgsV1({gasLimit: 300_000})
+            ),
+            // Set the feeToken to a feeTokenAddress, indicating specific asset will be used for fees
+            feeToken: _feeTokenAddress
+        });
+    }
+
+    /// @notice The entrypoint for the CCIP router to call. This function should
+    /// never revert, all errors should be handled internally in this contract.
+    /// @param any2EvmMessage The message to process.
+    /// @dev Extremely important to ensure only router calls this.
+    function ccipReceive(Client.Any2EVMMessage calldata any2EvmMessage) external override onlyRouter {
+        _ccipReceive(any2EvmMessage);
+    }
+
+    function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) internal override {
+        // Decode the message root from the peer
+        JBMessageRoot memory root = abi.decode(any2EvmMessage.data, (JBMessageRoot));
+        address origin = abi.decode(any2EvmMessage.sender, (address));
+
+        // Make sure that the message came from our peer.
+        if (origin != address(this)) revert NOT_PEER();
+
+        // Increase the outstanding amount to be added to the project's balance by the amount being received.
+        amountToAddToBalance[root.token] += root.amount;
+
+        // TODO: Is this necessary anymore?
+        // If the received tree's nonce is greater than the current inbox tree's nonce, update the inbox tree.
+        // We can't revert because this could be a native token transfer. If we reverted, we would lose the native tokens.
+        if (root.remoteRoot.nonce > inbox[root.token].nonce) {
+            inbox[root.token] = root.remoteRoot;
+            emit NewInboxTreeRoot(root.token, root.remoteRoot.nonce, root.remoteRoot.root);
+        }
+    }
+
+    /// @notice Checks if the `sender` (`msg.sender`) is a valid representative of the remote peer.
+    /// @param sender The message's sender.
+    function _isRemotePeer(address sender) internal view override onlyRouter returns (bool _valid) {
+        // Checks modifier onlyRouter and returns true if passing.
+        return true;
+    }
+
+    /// @notice Returns the chain on which the peer is located.
+    /// @return chainId of the peer.
+    function peerChainID() external view virtual override returns (uint256 chainId) {
+        // Return the remote chain id
+        return remoteChainId;
+    }
+}
---
+++ src/deployers/JBCCIPSuckerDeployer.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {JBPermissioned} from "@bananapus/core/src/abstract/JBPermissioned.sol";
+import {IJBPermissions} from "@bananapus/core/src/interfaces/IJBPermissions.sol";
+import {IJBPrices} from "@bananapus/core/src/interfaces/IJBPrices.sol";
+import {IJBDirectory} from "@bananapus/core/src/interfaces/IJBDirectory.sol";
+import {IJBRulesets} from "@bananapus/core/src/interfaces/IJBRulesets.sol";
+import {IJBTokens} from "@bananapus/core/src/interfaces/IJBTokens.sol";
+import {JBCCIPSucker} from "../JBCCIPSucker.sol";
+import {JBAddToBalanceMode} from "../enums/JBAddToBalanceMode.sol";
+import {IJBSucker} from "./../interfaces/IJBSucker.sol";
+import {IJBSuckerDeployer} from "./../interfaces/IJBSuckerDeployer.sol";
+
+/// @notice An `IJBSuckerDeployer` implementation to deploy contracts.
+contract JBCCIPSuckerDeployer is JBPermissioned, IJBSuckerDeployer {
+    error ONLY_ADMIN();
+    error ONLY_SUCKERS();
+    error ALREADY_CONFIGURED();
+    error NOT_CONFIGURED();
+
+    /// @notice The directory of terminals and controllers for projects.
+    IJBDirectory immutable DIRECTORY;
+
+    /// @notice The contract that manages token minting and burning.
+    IJBTokens immutable TOKENS;
+
+    /// @notice Only this address can configure this deployer, can only be used once.
+    address immutable LAYER_SPECIFIC_CONFIGURATOR;
+
+    /// @notice A mapping of suckers deployed by this contract.
+    mapping(address => bool) public isSucker;
+
+    /// @notice A temporary storage slot used by suckers to maintain deterministic deploys.
+    uint256 public TEMP_ID_STORE;
+
+    /// @notice Store the remote chain id
+    uint256 public REMOTE_CHAIN_ID;
+
+    /// @notice Store the remote chain id
+    uint64 public REMOTE_CHAIN_SELECTOR;
+
+    constructor(IJBDirectory directory, IJBTokens tokens, IJBPermissions permissions, address _configurator)
+        JBPermissioned(permissions)
+    {
+        LAYER_SPECIFIC_CONFIGURATOR = _configurator;
+        DIRECTORY = directory;
+        TOKENS = tokens;
+    }
+
+    /// @notice Create a new `JBSucker` for a specific project.
+    /// @dev Uses the sender address as the salt, which means the same sender must call this function on both chains.
+    /// @param localProjectId The project's ID on the local chain.
+    /// @param salt The salt to use for the `create2` address.
+    /// @return sucker The address of the new sucker.
+    function createForSender(uint256 localProjectId, bytes32 salt) external returns (IJBSucker sucker) {
+        // Check layer specific properties first
+        if (REMOTE_CHAIN_ID == 0 || REMOTE_CHAIN_SELECTOR == 0) revert NOT_CONFIGURED();
+
+        salt = keccak256(abi.encodePacked(msg.sender, salt));
+
+        // Set for a callback to this contract.
+        TEMP_ID_STORE = localProjectId;
+
+        sucker = IJBSucker(
+            address(new JBCCIPSucker{salt: salt}(DIRECTORY, TOKENS, PERMISSIONS, address(0), JBAddToBalanceMode.MANUAL))
+        );
+
+        // TODO: See if resetting this value is cheaper than deletion
+        // Delete after callback should complete.
+        /* delete TEMP_ID_STORE; */
+
+        isSucker[address(sucker)] = true;
+    }
+
+    /// @notice handles some layer specific configuration that can't be done in the constructor otherwise deployment addresses would change.
+    function configureLayerSpecific(uint256 remoteChainId, uint64 remoteChainSelector) external {
+        // TODO: Update if necessary
+
+        // Only allow configurator to set properties - notice we don't restrict reconfiguration here
+        if (msg.sender != LAYER_SPECIFIC_CONFIGURATOR) revert ONLY_ADMIN();
+
+        REMOTE_CHAIN_ID = remoteChainId;
+        REMOTE_CHAIN_SELECTOR = remoteChainSelector;
+    }
+}
