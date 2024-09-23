9c1b7865f172507e3d783e3afcbfd82d2dcbbcb8
simplemachine92 <justinpulley92@gmail.com>
2024-09-23 16:50:03 -0500

    moving files for based

---
+++ src/interfaces/IJBCCIPSuckerDeployer.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IJBSucker} from "./IJBSucker.sol";
+
+interface IJBCCIPSuckerDeployer {
+    function createForSender(uint256 localProjectId, bytes32 salt) external returns (IJBSucker sucker);
+
+    function TEMP_ID_STORE() external view returns (uint256);
+
+    function REMOTE_CHAIN_ID() external view returns (uint256);
+
+    function REMOTE_CHAIN_SELECTOR() external view returns (uint64);
+}
