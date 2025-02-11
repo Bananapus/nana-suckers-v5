690bb1d017f3bc3c8f692fb9fe61a32624b34de6
mejango <mejango@protonmail.com>
2025-02-11 10:34:27 -0300

    order

--- src/interfaces/IJBSuckerRegistry.sol
+++ src/interfaces/IJBSuckerRegistry.sol
@@ -15,9 +15,9 @@ interface IJBSuckerRegistry {
     function DIRECTORY() external view returns (IJBDirectory);
     function PROJECTS() external view returns (IJBProjects);
 
-    function suckerPairsOf(uint256 projectId) external view returns (JBSuckersPair[] memory pairs);
     function isSuckerOf(uint256 projectId, address addr) external view returns (bool);
     function suckerDeployerIsAllowed(address deployer) external view returns (bool);
+    function suckerPairsOf(uint256 projectId) external view returns (JBSuckersPair[] memory pairs);
     function suckersOf(uint256 projectId) external view returns (address[] memory);
 
     function allowSuckerDeployer(address deployer) external;
